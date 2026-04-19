e1b98a62e929549e4251c01c2734d8ecc18952b9
Manish Verma <manishverma201197@gmail.com>
2026-04-19 17:08:42 +0530

    fix errors

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -11,10 +11,10 @@ const Footer = () => {
             <div className='socialIcons'>
                 <a href="https://mail.google.com/mail/?view=cm&fs=1&to=manishverma201197@gmail.com"
                     target="_blank"  rel="noopener noreferrer"> <SiGmail /> </a>
-                <a href='https://www.linkedin.com/in/manishverma007/' target='_blank'> <FaLinkedin /> </a>
-                <a href="https://www.instagram.com/__maniiish.___/" target="_blank"><FaInstagram /></a>
-                <a href="https://www.facebook.com/profile.php?id=100074037405713" target="_blank"><FaFacebook /></a>
-                <a href="https://www.youtube.com/@Jerryy0007" target="_blank"><FaYoutube /></a>
+                <a href='https://www.linkedin.com/in/manishverma007/' target='_blank' rel="noreferrer"> <FaLinkedin /> </a>
+                <a href="https://www.instagram.com/__maniiish.___/" target="_blank" rel="noreferrer"><FaInstagram /></a>
+                <a href="https://www.facebook.com/profile.php?id=100074037405713" target="_blank" rel="noreferrer"><FaFacebook /></a>
+                <a href="https://www.youtube.com/@Jerryy0007" target="_blank" rel="noreferrer"><FaYoutube /></a>
             </div>
 
             <div id='fPara'>
