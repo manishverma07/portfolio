635d73ec63eac5e8006df8b9eaa4fa888d02db8d
Manish Verma <manishverma201197@gmail.com>
2026-04-22 14:27:05 +0530

    hamburger added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react'
-
+import { useState } from 'react'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     return (
 
         <nav className='container navbar'>
@@ -10,13 +11,18 @@ const Navbar = () => {
                 <a href='#home'> Manish Verma  </a>
             </div>
 
-            <ul>
+            <ul className={open ? "nav-links active" : "nav-links"}>
                 <li> <a href='#home'> Home </a> </li>
                 <li> <a href='#about'> About </a> </li>
                 <li> <a href='#skills'> Skills </a>  </li>
                 <li> <a href='#projects'> Projects </a> </li>
                 <li> <a href='#contact'> Contact </a> </li>
             </ul>
+
+            <div className='hamburger' onClick={() => setOpen(prev => !prev)}>
+                <svg fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M2 11H22V13H2zM2 5H22V7H2zM2 17H22V19H2z" /></svg>
+            </div>
+
 
         </nav>
     )
