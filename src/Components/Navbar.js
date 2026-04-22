69409f1a69c1cd0fdbd0235e3e1ccf63838b300a
Manish Verma <manishverma201197@gmail.com>
2026-04-22 16:23:21 +0530

    updated hamburger

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 
 const Navbar = () => {
-    const [open, setOpen] = useState(false);
+const [open, setOpen] = useState (false);
     return (
 
         <nav className='container navbar'>
@@ -19,7 +19,7 @@ const Navbar = () => {
                 <li> <a href='#contact'> Contact </a> </li>
             </ul>
 
-            <div className='hamburger' onClick={() => setOpen(prev => !prev)}>
+            <div className='hamburger' onClick={()=>setOpen (prev=> !prev)}>
                 <svg fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M2 11H22V13H2zM2 5H22V7H2zM2 17H22V19H2z" /></svg>
             </div>
 
